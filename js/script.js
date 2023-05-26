4176a0bf0ac5fdab18d0d84a2eaf6488882e3d35
Yvette Rosario <vette.rosario@gmail.com>
2023-05-25 23:45:10 -0400

    socials carousel + upcoming events sections

--- js/script.js
+++ js/script.js
@@ -1,66 +1,29 @@
-const cardData = [
-    {
-      image: 'image-url-1.jpg',
-      artist: 'Artist 1',
-      showName: 'Show 1',
-      date: '2023-05-25'
-    },
-    {
-      image: 'image-url-2.jpg',
-      artist: 'Artist 2',
-      showName: 'Show 2',
-      date: '2023-05-26'
-    },
-    {
-      image: 'image-url-3.jpg',
-      artist: 'Artist 3',
-      showName: 'Show 3',
-      date: '2023-05-27'
-    }
-  ];
 
-  function createCard(card) {
-    const container = document.getElementById('cardContainer');
+// social feed carousel
 
-    // Create card element
-    const cardElement = document.createElement('div');
-    cardElement.classList.add('card');
+let slideIndex = 1;
+showSlides(slideIndex);
 
-    // Add image
-    const imageElement = document.createElement('img');
-    imageElement.src = card.image;
-    cardElement.appendChild(imageElement);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-    // Add artist
-    const artistElement = document.createElement('div');
-    artistElement.classList.add('artist');
-    artistElement.textContent = card.artist;
-    cardElement.appendChild(artistElement);
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-    // Add show name
-    const titleElement = document.createElement('div');
-    titleElement.classList.add('title');
-    titleElement.textContent = card.showName;
-    cardElement.appendChild(titleElement);
-
-    const dateElement = document.createElement('div');
-    dateElement.classList.add('date');
-
-    const dateObj = new Date(card.date);
-    const options = { month: 'long', day: 'numeric', year: 'numeric' };
-    const dateString = dateObj.toLocaleDateString('en-US', options);
-    dateElement.textContent = dateString;
-    cardElement.appendChild(dateElement);
-
-    // Add weekday
-    const weekdayElement = document.createElement('div');
-    weekdayElement.classList.add('weekday');
-    weekdayElement.textContent = dateObj.toLocaleDateString('en-US', { weekday: 'long' });
-    cardElement.appendChild(weekdayElement);
-
-    // Append card to container
-    container.appendChild(cardElement);
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-
-  // Create cards using data
-  cardData.forEach(createCard);
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
