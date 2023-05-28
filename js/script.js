f35a5c40d987653e55642e4bc554db1c77894e67
Yvette Rosario <vette.rosario@gmail.com>
2023-05-27 22:31:51 -0400

    all elements mobile responsive

--- js/script.js
+++ js/script.js
@@ -27,3 +27,33 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+// dropdown menu
+
+class DropDown {
+  constructor() {
+    this.isMenuOpen = false;
+    this.dropdownToggle = document.querySelector('.dropdown-toggle');
+    this.dropdownMenu = document.querySelector('.dropdown-menu');
+
+    this.dropdownToggle.addEventListener('click', this.toggleMenu.bind(this));
+    document.addEventListener('click', this.closeMenu.bind(this));
+  }
+
+  toggleMenu(event) {
+    event.stopPropagation();
+    this.isMenuOpen = !this.isMenuOpen;
+    this.dropdownToggle.classList.toggle('active');
+    this.dropdownMenu.style.display = this.isMenuOpen ? 'block' : 'none';
+  }
+
+  closeMenu(event) {
+    if (!event.target.closest('.nav-header')) {
+      this.isMenuOpen = false;
+      this.dropdownToggle.classList.remove('active');
+      this.dropdownMenu.style.display = 'none';
+    }
+  }
+}
+
+new DropDown();
