0088bc97c843491913ec703ab9bc5770d3bcc968
Yvette Rosario <vette.rosario@gmail.com>
2023-05-30 15:02:50 -0400

    directions section base, still needs adjusting

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-
+function initMap() {
+  let uluru = {lat: -25.344, lng: 131.036};
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: uluru
+  });
+  let marker = new google.maps.Marker({position: uluru, map: map});
+}
 
 
 // dropdown menu
