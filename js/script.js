fda96ddcff928f2d53202ad79e4bed40841ba12d
Yvette Rosario <vette.rosario@gmail.com>
2023-05-30 14:47:58 -0400

    social media tabs / touch carousel working

--- js/script.js
+++ js/script.js
@@ -10,3 +10,37 @@ function myFunction() {
       x.style.display = "block";
   }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  const cardData = [
+    {
+      heading: '@ CHVENUE',
+      body: 'Catch unforgettable music moments @ acllive and @3TENaustin in Austin, TX. With over 130 concerts per year, discover world-renowned and local artists up close! #ACLLIVE #3TENACLLIVE'
+    },
+    {
+      heading: '@ CHVENUE',
+      body: "Tickets for @Thundercat's IOn Yo Girl's City Tour go on sale tomorrow/ 5/5 @ 10AM local time!! Get yours here"
+    },
+    {
+      heading: '@ CHVENUE',
+      body: "Tickets for blah blah blah blah blah blah blah blah blah blah blah I can't see the rest blah blah blah"
+    },
+
+  ];
+
+  const postContainer = document.querySelector('.socials-card-container');
+
+  const postMethods = () => {
+    cardData.map((postData) => {
+      const postElement = document.createElement('div');
+      postElement.classList.add('socials-card');
+      postElement.innerHTML = `
+        <p class="card-heading"><i class="fa fa-twitter"></i>${postData.heading}</p>
+        <p class="card-body">${postData.body}</p>
+      `;
+      postContainer.appendChild(postElement);
+    });
+  };
+
+  postMethods();
+});
